1725a6eb71b8ea822451c95f47eef4eb37547899
Hamza Ahmad Khan <ihamzahmadkhan@gmail.com>
2025-01-29 10:34:23 -0800

    dsa

---
+++ labs.py
@@ -0,0 +1,3 @@
+print("My lucky numbers are", 3*4+5, 5*6-1)
+
+print(1/0)
---
+++ squratroot.py
@@ -0,0 +1,11 @@
+# Initialize x
+x=1.0
+print("x*x:", x*x)
+# First iteration 
+delta = (1/x)-x/2 
+x = x+delta  
+print("x*x:",x*x)
+# Second iteration 
+delta = (1/x)-x/2
+x = x+delta
+print("x*x:", x*x)
